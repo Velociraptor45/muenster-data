c326fe6958f072cbd2a4c0589c1b083c738ddb9d
Velociraptor45 <30436688+Velociraptor45@users.noreply.github.com>
2025-03-30 11:05:11 +0200

    Add correct colors for pie chart

--- src/MuensterData.Domain/Politics/States/Turnout.cs
+++ src/MuensterData.Domain/Politics/States/Turnout.cs
@@ -5,7 +5,19 @@ public record Turnout(int Eligible, int Actual)
     public decimal Percentage = Actual / (decimal)Eligible * 100;
     public IReadOnlyCollection<object> FormatForChart =
     [
-        new { Name = "Gewählt", Count = (Actual / (decimal)Eligible * 100), Label = $"Gewählt: {(Actual /(decimal) Eligible * 100):##.00}%" },
-        new { Name = "Nicht gewählt", Count = 100 - (Actual / (decimal)Eligible * 100), Label = $"Nicht gewählt: {100 - (Actual /(decimal) Eligible * 100):##.00}%" }
+        new
+        {
+            Name = "Gewählt",
+            Count = (Actual / (decimal)Eligible * 100),
+            Label = $"Gewählt: {(Actual /(decimal) Eligible * 100):##.00}%",
+            Fill = "#007a99"
+        },
+        new
+        {
+            Name = "Nicht gewählt",
+            Count = 100 - (Actual / (decimal)Eligible * 100),
+            Label = $"Nicht gewählt: {100 - (Actual /(decimal) Eligible * 100):##.00}%",
+            Fill = "#c4c91f"
+        }
     ];
 };
